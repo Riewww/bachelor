fae0912db2d5c205b7c5c98750210fa0caac7c8c
rieke <rieke.wallaschkowski@stud-hs-emden-leer.de>
2022-02-17 12:18:59 +0100

    drop fake entrys  from all species

---
+++ droppingfakes.py
@@ -0,0 +1,31 @@
+import database
+import os
+
+#loads all file names of directory into one list
+listreal = os.listdir("/Users/rieke/Desktop/real/")
+listall = os.listdir("/Users/rieke/Desktop/ortho/OrthoFinder/Results_Feb16/Orthogroup_Sequences/")
+
+#ids that are not interleukins and need to be dropped
+drop = []
+
+connection = database.create_connection_own()
+
+
+# checks if OrthoGroup name is in directory with "real ones"
+# if not in there, saves all stable ids in drop
+# drops all entrys with stable id in drop from all species
+def removefiles():
+    for names in listall:
+        if (names.replace('.fa','.txt')) in listreal:
+            continue
+        else:
+            file = open("/Users/rieke/Desktop/ortho/OrthoFinder/Results_Feb16/Orthogroup_Sequences/" + names.replace('.txt',".fa"), 'r' )
+            for lines in file:
+                if lines.startswith('>'):
+                    drop.append((lines).replace(">","").strip())
+    for ids in drop:
+        continue
+#database.execute_query(connection, "DELETE FROM all_species WHERE id = '" + id + "';")
+# #connection.commit()
+
+removefiles()
